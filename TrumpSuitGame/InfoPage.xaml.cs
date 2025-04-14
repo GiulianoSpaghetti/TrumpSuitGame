c2de58f40d41bc168b6f5fade1960e5fdfa9fa8f
Giulio Sorrentino <numerone@fastwebnet.it>
2025-04-14 17:28:50 +0200

    miglioramenti alla stabilità e all'usabilità

--- TrumpSuitGame/InfoPage.xaml.cs
+++ TrumpSuitGame/InfoPage.xaml.cs
@@ -2,14 +2,16 @@ namespace TrumpSuitGame;
 
 public partial class InfoPage : ContentPage
 {
-	public InfoPage()
+    public static readonly Uri uri = new Uri("https://github.com/GiulianoSpaghetti/TrumpSuitGame");
+
+    public InfoPage()
 	{
 		InitializeComponent();
         Title = $"{App.d["Informazioni"]}";
     }
     private async void OnSito_Click(object sender, EventArgs e)
     {
-        await Launcher.Default.OpenAsync(new Uri("https://github.com/GiulianoSpaghetti/TrumpSuitGame"));
+        await Launcher.Default.OpenAsync(uri);
     }
 
 }
